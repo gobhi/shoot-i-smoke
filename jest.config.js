f2402a1796871cb9474e9c8ba6829aa99b06ad3e
Amaury Martiny <amaury.martiny@protonmail.com>
2019-11-27 11:59:10 +0100

    Make detox run with failing tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  preset: 'jest-expo',
-  moduleFileExtensions: ['js', 'ts', 'tsx'],
-  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js' // https://github.com/facebook/react-native/issues/21075#issuecomment-441332894
-  }
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupTestFrameworkScriptFile: './e2e/testSetup.ts'
 };
