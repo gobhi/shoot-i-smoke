a7e173de74409e082f79ce918645f14d9d947584
Amaury Martiny <amaury.martiny@protonmail.com>
2020-01-10 17:34:53 +0100

    Add testIDs

--- e2e/firstTest.spec.ts
+++ e2e/firstTest.spec.ts
@@ -1,22 +1,14 @@
 import { by, element, expect } from 'detox';
 import { reloadApp } from 'detox-expo-helpers';
 
+import { testIds } from '../App/util/testId';
+
 describe('Example', () => {
   beforeEach(async () => {
     await reloadApp();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it('should have "cough..." text', async () => {
+    await expect(element(by.id(testIds.Loading.coughText))).toBeVisible();
   });
 });
